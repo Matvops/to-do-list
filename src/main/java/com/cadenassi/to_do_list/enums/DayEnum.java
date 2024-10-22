46b8aed0b51045bd085cac5fc77012a19e207836
Matheus <matheusmartinscia@gmail.com>
2024-10-22 07:47:51 -0300

    DayEnum implementado

--- src/main/java/com/cadenassi/to_do_list/enums/DayEnum.java
+++ src/main/java/com/cadenassi/to_do_list/enums/DayEnum.java
@@ -1,4 +1,32 @@
 package com.cadenassi.to_do_list.enums;
 
 public enum DayEnum {
+
+    DOMINGO(1),
+    SEGUNDA(2),
+    TERCA(3),
+    QUARTA(4),
+    QUINTA(5),
+    SEXTA(6),
+    SABADO(7);
+
+    private int i;
+
+    private DayEnum(int i){
+        this.i = i;
+    }
+
+    public int getDayCode(){
+        return this.i;
+    }
+
+    public static DayEnum getDay(int i) {
+        DayEnum[] days = DayEnum.values();
+
+        for(DayEnum day: days)
+            if(day.getDayCode() == i)
+                return day;
+
+        return null;
+    }
 }

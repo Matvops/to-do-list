f4a284893ac9c20cab6c16347c54f68509a38b92
Matheus <matheusmartinscia@gmail.com>
2024-10-22 08:15:56 -0300

    TaskService criado

--- src/main/java/com/cadenassi/to_do_list/services/TaskService.java
+++ src/main/java/com/cadenassi/to_do_list/services/TaskService.java
@@ -1,4 +1,44 @@
 package com.cadenassi.to_do_list.services;
 
+import com.cadenassi.to_do_list.dto.TaskDTO;
+import com.cadenassi.to_do_list.enums.DayEnum;
+import com.cadenassi.to_do_list.mappers.TaskMapper;
+import com.cadenassi.to_do_list.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    @Autowired
+    private TaskMapper mapper;
+
+    public TaskDTO findAll(){
+
+    }
+
+    public TaskDTO findByDay(String day){
+
+    }
+
+    public TaskDTO insert(TaskDTO){
+
+    }
+
+    public TaskDTO update(TaskDTO taskDTO, String id){
+
+    }
+
+    public TaskDTO updateCompleted(TaskDTO taskDTO, String id){
+
+    }
+
+    public void delete(String day, String id){
+
+    }
+
+
 }

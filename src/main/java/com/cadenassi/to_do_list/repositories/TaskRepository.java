fd65c0cef08192554f13c413711b5b4ad4942c19
Matheus <matheusmartinscia@gmail.com>
2024-10-22 08:03:15 -0300

    Repository implementado

---
+++ src/main/java/com/cadenassi/to_do_list/repositories/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.cadenassi.to_do_list.repositories;
+
+import com.cadenassi.to_do_list.domain.Task;
+import com.cadenassi.to_do_list.enums.DayEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @Query("SELECT t FROM Task t WHERE t.day LIKE LOWER(CONCAT('%', :day, '%'))")
+    Page<Task> findByDayEnum(@Param("day")DayEnum day);
+}

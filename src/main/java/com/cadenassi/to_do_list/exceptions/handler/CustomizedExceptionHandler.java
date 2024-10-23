d44c54c9024c745fa209fe509a9e82578e0dca5e
Matheus <matheusmartinscia@gmail.com>
2024-10-23 07:28:37 -0300

    CustomizedExceptionHandler implementada

--- src/main/java/com/cadenassi/to_do_list/exceptions/handler/CustomizedExceptionHandler.java
+++ src/main/java/com/cadenassi/to_do_list/exceptions/handler/CustomizedExceptionHandler.java
@@ -1,4 +1,45 @@
 package com.cadenassi.to_do_list.exceptions.handler;
 
-public class CustomizedExceptionHandler g{
+import com.cadenassi.to_do_list.exceptions.ExceptionResponse;
+import com.cadenassi.to_do_list.exceptions.ObjectIsNullException;
+import com.cadenassi.to_do_list.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class CustomizedExceptionHandler{
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> exceptionHandler(Exception e, WebRequest webRequest){
+
+        ExceptionResponse response = new ExceptionResponse(Instant.now(), e.getMessage(), webRequest
+                .getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> resourceNotFoundException(ResourceNotFoundException e, WebRequest webRequest){
+
+        ExceptionResponse response = new ExceptionResponse(Instant.now(), e.getMessage(), webRequest
+                .getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(ObjectIsNullException.class)
+    public ResponseEntity<ExceptionResponse> ObjectIsNullException(ObjectIsNullException e, WebRequest webRequest){
+
+        ExceptionResponse response = new ExceptionResponse(Instant.now(), e.getMessage(), webRequest
+                .getDescription(false));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
 }

1f1011e0f33fcfc3b76239de5d7c99aa3b4d9b0a
Matheus <matheusmartinscia@gmail.com>
2024-10-24 06:35:32 -0300

    TaskService implementado

--- src/main/java/com/cadenassi/to_do_list/repositories/TaskRepository.java
+++ src/main/java/com/cadenassi/to_do_list/repositories/TaskRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Query("SELECT t FROM Task t WHERE t.day LIKE LOWER(CONCAT('%', :day, '%'))")
-    Task findByDayEnum(@Param("day")DayEnum day);
+    List<Task> findByDayEnum(@Param("day")DayEnum day);
 }
--- src/main/java/com/cadenassi/to_do_list/services/TaskService.java
+++ src/main/java/com/cadenassi/to_do_list/services/TaskService.java
@@ -2,13 +2,22 @@ package com.cadenassi.to_do_list.services;
 
 import com.cadenassi.to_do_list.dto.TaskDTO;
 import com.cadenassi.to_do_list.enums.DayEnum;
+import com.cadenassi.to_do_list.exceptions.ObjectIsNullException;
+import com.cadenassi.to_do_list.exceptions.ResourceNotFoundException;
 import com.cadenassi.to_do_list.mappers.TaskMapper;
 import com.cadenassi.to_do_list.repositories.TaskRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 @Service
 public class TaskService {
+
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(TaskService.class);
+    Logger logger = Logger.getLogger(TaskService.class.getName());
 
     @Autowired
     private TaskRepository repository;
@@ -16,29 +25,103 @@ public class TaskService {
     @Autowired
     private TaskMapper mapper;
 
-    public TaskDTO findAll(){
+    public List<TaskDTO> findAll(){
+        logger.info("Find all tasks");
 
+        var tasks = mapper.toDTOs(repository.findAll());
+
+        return tasks;
     }
 
-    public TaskDTO findByDay(String day){
+    public List<TaskDTO> findByDay(String day){
+        logger.info("Find dy day");
 
+        checkString(day);
+
+        DayEnum dayEnum = DayEnum.valueOf(day.toUpperCase());
+
+        var tasks = mapper.toDTOs(repository.findByDayEnum(dayEnum));
+
+        return tasks;
     }
 
-    public TaskDTO insert(TaskDTO){
+    public TaskDTO insert(TaskDTO taskDTO){
+        logger.info("Insert a task");
 
+        checkDTO(taskDTO);
+
+        var task = mapper.toDTO(repository.save(mapper.toTask(taskDTO)));
+
+        return task;
     }
 
     public TaskDTO update(TaskDTO taskDTO, String id){
+        logger.info("Update a Task");
 
+        checkDTO(taskDTO);
+        checkString(id);
+
+        var tasks = findByDay(taskDTO.getDay().name());
+
+        var dto = tasks.get(parseStringToInteger(id) - 1);
+
+        var task = repository.findById(dto.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("RESOURCE NOT FOUND!"));
+
+        task.setName(taskDTO.getName());
+        task.setDay(taskDTO.getDay());
+        task.setPriority(taskDTO.getPriority());
+
+        return mapper.toDTO(repository.save(task));
     }
 
-    public TaskDTO updateCompleted(TaskDTO taskDTO, String id){
+    public TaskDTO updateCompleted(String day, String id){
+        logger.info("Update variable Completed of Task");
 
+        checkString(day);
+        checkString(id);
+
+        var tasks = findByDay(day);
+
+        var dto = tasks.get(parseStringToInteger(id) - 1);
+
+        var task = repository.findById(dto.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("RESOURCE NOT FOUND!"));
+
+        task.setCompleted(!task.isCompleted());
+
+        return mapper.toDTO(repository.save(task));
     }
 
     public void delete(String day, String id){
+        logger.info("Delete a Task");
 
+        checkString(day);
+        checkString(id);
+
+        var tasks = findByDay(day);
+
+        var dto = tasks.get(parseStringToInteger(id) - 1);
+
+        repository.deleteById(dto.getId());
     }
+
+    private void checkString(String number) {
+        if (number == null) throw new ObjectIsNullException("OBJECT IS NULL!");
+        if (number.isBlank()) throw new ResourceNotFoundException("OBJECT IS EMPTY!");
+    }
+
+
+    private void checkDTO(TaskDTO task){
+        if (task == null) throw new ObjectIsNullException("OBJECT IS NULL!");
+    }
+
+    private Integer parseStringToInteger(String number) {
+        if (!number.matches("\\d*[0-9]\\d*")) throw new IllegalArgumentException("NOT IS INTEGER!");
+
+        return Integer.parseInt(number);
+    }
+
 
 
 }

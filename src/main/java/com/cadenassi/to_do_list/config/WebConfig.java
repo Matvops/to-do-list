7c73973f31467e50fc7682f7f48234ff25a50aef
Matheus <matheusmartinscia@gmail.com>
2024-10-25 08:03:01 -0300

    Content negotiation implementado

---
+++ src/main/java/com/cadenassi/to_do_list/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.cadenassi.to_do_list.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .defaultContentType(MediaType.valueOf(MediaType.APPLICATION_JSON_VALUE));
+    }
+}

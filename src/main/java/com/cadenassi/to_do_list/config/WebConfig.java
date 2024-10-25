042a2d7f66074e8b54cf8220c072228cec9c9a64
Matheus <matheusmartinscia@gmail.com>
2024-10-25 08:03:36 -0300

    Classe de configuração WebConfig

--- src/main/java/com/cadenassi/to_do_list/config/WebConfig.java
+++ src/main/java/com/cadenassi/to_do_list/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.cadenassi.to_do_list.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override

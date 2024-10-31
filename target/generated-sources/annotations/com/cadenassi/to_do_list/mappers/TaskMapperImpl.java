52a5ff4f4c15304b2351d0755d6c86379728c03d
Matheus <matheusmartinscia@gmail.com>
2024-10-30 23:10:20 -0300

    Start dockerization

--- target/generated-sources/annotations/com/cadenassi/to_do_list/mappers/TaskMapperImpl.java
+++ target/generated-sources/annotations/com/cadenassi/to_do_list/mappers/TaskMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-30T13:36:09-0300",
-    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
+    date = "2024-10-30T22:59:00-0300",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
 public class TaskMapperImpl implements TaskMapper {
